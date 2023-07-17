e019ac483dae9883abce585baa4ef5e37b7112de
iturres <emanueliturres.co@gmail.com>
2023-07-17 20:59:55 -0300

    Desctructure 'CartItem' payload and add increase/decrease reducers

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 
-const CartItem = (cartItem) => {
+const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch();
 
   return (
     <article className="cart-item">
-      <img src={cartItem.img} alt={cartItem.title} />
+      <img src={img} alt={title} />
       <div>
-        <h4>{cartItem.title}</h4>
-        <h4 className="item-price">${cartItem.price}</h4>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
         <button
           className="remove-btn"
           onClick={() => {
-            dispatch(removeItem(cartItem.id));
+            dispatch(removeItem(id));
           }}
         >
           remove
         </button>
       </div>
       <div>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
           <ChevronUp />
         </button>
-        <p className="amount">{cartItem.amount}</p>
-        <button className="amount-btn">
+        <p className="amount">{amount}</p>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -19,8 +19,20 @@ const cartSlice = createSlice({
       const itemId = action.payload;
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
+    increase: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload.id,
+      );
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload.id,
+      );
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
