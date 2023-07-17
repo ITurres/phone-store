7b4aeb9279c141c2ce2496bba7da24c8027ef60b
iturres <emanueliturres.co@gmail.com>
2023-07-17 19:54:42 -0300

    feat: Added dispatch to 'removeItem's on 'CartItem' -component-

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
+import { removeItem } from '../features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const CartItem = (cartItem) => {
+  const dispatch = useDispatch();
+
   return (
     <article className="cart-item">
       <img src={cartItem.img} alt={cartItem.title} />
       <div>
         <h4>{cartItem.title}</h4>
         <h4 className="item-price">${cartItem.price}</h4>
-        <button className="remove-btn">remove</button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(cartItem.id));
+          }}
+        >
+          remove
+        </button>
       </div>
       <div>
         <button className="amount-btn">
