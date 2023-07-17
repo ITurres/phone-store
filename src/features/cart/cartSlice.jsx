e417864098e697fa5ab8d4f3d6859f6003de21f5
iturres <emanueliturres.co@gmail.com>
2023-07-17 19:55:37 -0300

    feat: Added reducers 'clearCart' && 'removeItem' to 'cartSlice'

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -11,8 +11,16 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
 });
 
-// ? console.log(cartSlice);
-
+export const { clearCart, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
