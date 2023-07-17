55ce07b0991b9d72db1d2d8e63d4eb581a72df23
iturres <emanueliturres.co@gmail.com>
2023-07-17 18:42:42 -0300

    feat: Add all setup files, components, features, data, etc

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from './components/Navbar';
+
+export const App = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+
+export const Navbar = () => {
+  const { amount } = useSelector((state) => state.cart); //? state = store
+
+  return (
+    <nav className="nav-center">
+      <h3>Phone Store</h3>
+      <div className="nav-container">
+        <CartIcon />
+        <div className="amount-container">
+          <p className="total-amount">{amount}</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
